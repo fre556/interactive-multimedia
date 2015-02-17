fa835ff64461e713f0f0457b8dd234d37790bf0e
Derek Caulfield <fre556@yahoo.com>
2015-02-17 09:45:22 +0000

    V 1.0 Gravity guy files added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private PlayerDisplay playerDisplay;
+	
+	private int score = 0;
+	private float deathY = -15;
+	
+	void Start(){
+		playerDisplay = GetComponent<PlayerDisplay>();
+		playerDisplay.UpdateScoreText(score);
+	}
+	
+	void Update(){
+		float y = transform.position.y;
+		if(y < deathY){
+			MoveToStartPosition();
+		}
+	}
+	
+	private void MoveToStartPosition(){
+		Vector3 startPosition = new Vector3(0,5,0);
+		transform.position = startPosition;
+	}
+	
+	void OnTriggerEnter2D(Collider2D hit){
+		if(hit.CompareTag("Food")){
+			score++;
+			playerDisplay.UpdateScoreText(score);
+			Destroy (hit.gameObject);
+			audio.Play();
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerDisplay.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerDisplay : MonoBehaviour {
+	public Text scoreText;
+	
+	public void UpdateScoreText(int newScore){
+		string scoreMessage = "Score = " + newScore;
+		scoreText.text = scoreMessage;
+	}
+}
